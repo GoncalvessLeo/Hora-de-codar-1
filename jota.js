a9771ffebe2dfdb399328196c4beb3d1f28c093a
Leonardo Sousa <125033731+GoncalvessLeo@users.noreply.github.com>
2023-03-10 12:37:01 -0300

    Add files via upload

---
+++ jota.js
@@ -0,0 +1,69 @@
+//Atividade 1
+
+// var nome_do_carro = "fusca";
+// document.write (nome_do_carro);
+
+
+//Atividade 2
+
+// var nome = prompt("Escreva o seu nome");
+// document.write ("Olá " , nome);
+
+
+//Atividade 3 
+
+// var nome = prompt("Escreva o seu nome");
+// var idade = prompt("Informe sua idade");
+// document.write ("<h1> Olá " , nome , " sua idade é " , idade , "</h1");
+
+//Atividade 4
+
+//Retângulo:
+
+// var base = prompt("Número da base ");
+// var altura = prompt("Número da altura ");
+// var retangulo = base * altura;
+// document.write (retangulo);
+
+//Quadrado:
+
+// var lado = prompt("Número do lado ")
+// var quadrado = lado * lado; 
+// document.write (quadrado);
+
+//Losango:
+// var diagonalM = parseInt(prompt("Número diagonal maior "));
+// var diagonalm = parseInt(prompt("Número diagonal menor "));
+// var losango = diagonalM * diagonalm / 2;
+// document.write(losango);
+
+//Trapézio:
+
+// var baseMaior = parseInt(prompt("Número da base maior "));
+// var baseMenor = parseInt(prompt("Número da base menor "));
+// var altura = prompt("Número da altura ");
+// var trapezio = baseMaior + baseMenor * altura /2;
+// document.write (trapezio);
+
+//Paralelogramo:
+
+//  var base = prompt("Número da base ");
+//  var altura = prompt("Número da altura ");
+//  var paralelogramo = base * altura;
+//  document.write (paralelogramo);
+
+//Triângulo:
+
+//   var base = prompt("Número da base ");
+//   var altura = prompt("Número da altura ");
+//   var triangulo = base * altura /2;
+//   document.write (triangulo);
+
+//Circulo:
+
+// var pi = 3.14;
+// var raio = parseInt(prompt("Número do raio "));
+// var circulo = pi * raio * raio;
+// document.write (circulo);
+
+
